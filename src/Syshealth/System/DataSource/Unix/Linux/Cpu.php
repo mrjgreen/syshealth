2231736253e219a69c2e417261e824ecd999f8f8
Joe Green <joe.green@flashtalking.com>
2014-11-24 11:21:45 +0000

    fix cpu parser

--- src/Syshealth/System/DataSource/Unix/Linux/Cpu.php
+++ src/Syshealth/System/DataSource/Unix/Linux/Cpu.php
@@ -7,6 +7,8 @@ use Syshealth\System\Entity\CpuEntity;
 
 class Cpu extends DataSourceAbstract
 {
+    private $lastCalulation = array();
+
     public function __construct()
     {
         parent::__construct('cat /proc/stat', new CpuTableParser());
@@ -16,18 +18,38 @@ class Cpu extends DataSourceAbstract
     {
         $data = $this->readData();
 
-        $cpus = array_map(function($row){
-            return $this->createEntity($row);
-        }, $data);
+        $cpus = array();
+        foreach($data as $i => $row)
+        {
+            $cpus[$i] = $this->createEntity($i, $row);
+        }
 
         return array(array_shift($cpus), $cpus);
     }
 
-    private function createEntity($row)
+    private function createEntity($i, $row)
     {
-        $pcent = $this->calculatePercentage(array_slice($row, 1));
+        $dataValues = array_slice($row, 1);
+
+        $averages = $this->calculateAverages($i, $dataValues);
+
+        $pcent = $this->calculatePercentage($averages);
 
         return new CpuEntity($row[0], $pcent[0], $pcent[1], $pcent[2], $pcent[4], $pcent[5], $pcent[6], $pcent[7], $pcent[8], $pcent[3]);
+    }
+
+    private function calculateAverages($i, $data)
+    {
+        foreach($data as $count => $value)
+        {
+            if(!isset($this->lastCalulation[$i][$count])) $this->lastCalulation[$i][$count] = 0;
+
+            $data[$count] = $value - $this->lastCalulation[$i][$count];
+
+            $this->lastCalulation[$i][$count] = $value;
+        }
+
+        return $data;
     }
 
     private function calculatePercentage($row)
