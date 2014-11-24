b7cfe3b32f00e8c7e30d57aec1354bbee333a2aa
Joe Green <joe.green@flashtalking.com>
2014-11-24 10:02:22 +0000

    use proc/stat for cpu data

--- src/Syshealth/System/DataParser/CpuTableParser.php
+++ src/Syshealth/System/DataParser/CpuTableParser.php
@@ -8,7 +8,7 @@ class CpuTableParser extends TableParser
          * cpu 525767 5 322183 6288550 258060 22 12420 0 0
          * cpu0 525767 5 322183 6288550 258060 22 12420 0 0
          */
-        preg_match('^(?:cpu(\d+)|(cpu))\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)(?:\s+(\d+))?(?:\s+(\d+))?', $line, $matches);
+        preg_match('/^(?:cpu(\d+)|(cpu))\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)(?:\s+(\d+))?(?:\s+(\d+))?/', $line, $matches);
 
         return array_slice($matches, 1) ?: false;
     }
